9029fe3cc915cfbbf12ea448a5dda3286f784d2b
Nemanja Cetkovic <ncetkovic@uns.ac.rs>
2019-02-08 07:20:02 +0100

    mail

--- Controllers/SeatReservationController.cs
+++ Controllers/SeatReservationController.cs
@@ -9,6 +9,8 @@ using ISA.Data;
 using ISA.Models.Entities;
 using Microsoft.AspNetCore.Identity;
 using ISA.Models.SeatReservationViewModels;
+using System.Net.Mail;
+using System.Net;
 
 namespace ISA.Controllers
 {
@@ -18,7 +20,7 @@ namespace ISA.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
 
         public SeatReservationController(
-            UserManager<ApplicationUser> userManager, 
+            UserManager<ApplicationUser> userManager,
             ApplicationDbContext context)
         {
             _userManager = userManager;
@@ -29,7 +31,7 @@ namespace ISA.Controllers
         public async Task<IActionResult> Index()
         {
             var user = _context.Users.Find((await _userManager.GetUserAsync(HttpContext.User)).Id);
-            List<SeatReservation> seatReservations = await _context.SeatReservations.Where(s=>s.ApplicationUser == user).ToListAsync();
+            List<SeatReservation> seatReservations = await _context.SeatReservations.Where(s => s.ApplicationUser == user).ToListAsync();
             return View(seatReservations);
         }
 
@@ -41,14 +43,16 @@ namespace ISA.Controllers
         {
             var user = _context.Users.Find((await _userManager.GetUserAsync(HttpContext.User)).Id);
             Flight flight = _context.Flights.Find(viewModel.FlightName);
-            _context.Entry(flight).Reference(f=>f.Airplane).Load();
-            _context.Entry(flight.Airplane).Reference(f=>f.Airline).Load();
+            _context.Entry(flight).Reference(f => f.Airplane).Load();
+            _context.Entry(flight.Airplane).Reference(f => f.Airline).Load();
 
-            if (flight == null) {
+            if (flight == null)
+            {
                 return NotFound();
             };
             Seat seat = _context.Seats.Find(flight.Airplane.AirplaneName, viewModel.SegmentName, viewModel.SeatName);
-            if(seat == null)
+            _context.Entry(seat).Reference(s => s.Segment).Load();
+            if (seat == null)
             {
                 return NotFound();
             }
@@ -65,7 +69,8 @@ namespace ISA.Controllers
 
             if (reservation == null)
             {
-                reservation = new Reservation {
+                reservation = new Reservation
+                {
                     Created = DateTime.Now,
                     ApplicationUser = user,
                     TotalPrice = flight.Price,
@@ -83,7 +88,8 @@ namespace ISA.Controllers
                 reservation.TotalPrice += flight.Price;
             }
 
-            SeatReservation seatReservation = new SeatReservation {
+            SeatReservation seatReservation = new SeatReservation
+            {
                 Flight = flight,
                 FirstName = viewModel.FirstName,
                 LastName = viewModel.LastName,
@@ -93,11 +99,29 @@ namespace ISA.Controllers
                 Seat = seat,
                 Reservation = reservation
             };
-            
+
             if (ModelState.IsValid)
             {
                 _context.Add(seatReservation);
                 await _context.SaveChangesAsync();
+
+                var smtpClient = new SmtpClient
+                {
+                    Host = "smtp.gmail.com", // set your SMTP server name here
+                    Port = 587, // Port 
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential("isa.ftn.2018@gmail.com", "~4;L2wTtt}c3h)Zw")
+                };
+
+                using (var message = new MailMessage("isa.ftn.2018@gmail.com", user.Email)
+                {
+                    Subject = "New Reservation",
+                    Body = "You have reserved seat " + seat.SeatName + " segment " + seat.Segment.SegmentName + " flight "+flight.FlightName
+                })
+                {
+                    await smtpClient.SendMailAsync(message);
+                }
+
                 return RedirectToAction("Details", "Flight", new { id = flight.FlightName });
             }
             return Forbid();
@@ -176,12 +200,12 @@ namespace ISA.Controllers
             var user = _context.Users.Find((await _userManager.GetUserAsync(HttpContext.User)).Id);
             SeatReservation seatReservation = _context.SeatReservations.Where(s => s.SeatReservationId == viewModel.SeatReservationId).FirstOrDefault();
             _context.Entry(seatReservation).Reference(s => s.Flight).Load();
-            if(seatReservation == null)
+            if (seatReservation == null)
             {
                 return NotFound();
             }
 
-            if(viewModel.Answer)
+            if (viewModel.Answer)
             {
                 seatReservation.Confirmed = true;
                 if (ModelState.IsValid)
